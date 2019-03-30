58905eec4209f13a253b27eb21eb8ae92155e574
Dreamwalker <aristojeff@gmail.com>
2019-03-30 15:32:00 +0900

    Update

--- lib/main.dart
+++ lib/main.dart
@@ -55,10 +55,11 @@ class _MyHomePageState extends State<MyHomePage> {
                 );
               } else {
                 return ListView.builder(
-                  padding: EdgeInsets.only(top: 18.0),
+                    padding: EdgeInsets.only(top: 18.0),
                     itemCount: snapshot.data.documents.length,
-                    itemBuilder: (context, index){
-                      return _buildListItem(context, snapshot.data.documents[index]);
+                    itemBuilder: (context, index) {
+                      return _buildListItem(
+                          context, snapshot.data.documents[index]);
                     });
               }
             }),
@@ -71,37 +72,130 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 
-  Widget _buildListItem(BuildContext context, DocumentSnapshot document){
+  Widget _buildListItem(BuildContext context, DocumentSnapshot document) {
     return Card(
-      child:Column(
+      child: Column(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
           ListTile(
             leading: const Icon(Icons.memory),
-            title: Text("【 " + (document['borrowOrLend'] == "lend"?"貸":"借") +
-            " 】" + document['stuff']),
-            subtitle: Text(" 期限 : " + document['date'].toString().substring(0, 10)
-            + "\n 相手 : " + document['user']),
+            title: Text("【 " +
+                (document['borrowOrLend'] == "lend" ? "貸" : "借") +
+                " 】" +
+                document['stuff']),
+            subtitle: Text(" 期限 : " +
+                document['date'].toString().substring(0, 10) +
+                "\n 相手 : " +
+                document['user']),
           ),
-
           ButtonTheme.bar(
             child: ButtonBar(
               children: <Widget>[
                 FlatButton(
                   child: const Text("へんしゅう"),
-                  onPressed: (){
-                    Scaffold.of(context).showSnackBar(SnackBar(content: Text("編集"
-                        "ボタンを押しました")));
+                  onPressed: () {
+                    Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text("編集"
+                            "ボタンを押しました")));
                   },
                 ),
-
               ],
             ),
           )
-
         ],
       ),
     );
   }
+}
 
+class _FormData {
+  String borrowOrLend = "borrow";
+  String user;
+  String stuff;
+  DateTime date = DateTime.now();
+}
+
+class InputForm extends StatefulWidget {
+  @override
+  _InputFormState createState() => _InputFormState();
+}
+
+class _InputFormState extends State<InputForm> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  final _FormData _data = _FormData();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("かしかり入力"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.save),
+            onPressed: () {
+              Scaffold.of(context)
+                  .showSnackBar(new SnackBar(content: Text("保存ボタンを押しました")));
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.delete),
+            onPressed: () {
+              Scaffold.of(context)
+                  .showSnackBar(new SnackBar(content: Text("削除ボタンを押しました")));
+            },
+          )
+        ],
+      ),
+      body: SafeArea(
+          child: Form(
+        key: _formKey,
+        child: ListView(
+          children: <Widget>[
+            RadioListTile(
+                value: "borrow",
+                title: Text("借りた"),
+                groupValue: _data.borrowOrLend,
+                onChanged: (String value) {
+                  print("借りたを");
+                }),
+            RadioListTile(
+                value: "lend",
+                title: Text("貸した"),
+                groupValue: _data.borrowOrLend,
+                onChanged: (String value) {
+                  print("貸したを");
+                }),
+
+            TextFormField(
+              decoration: new InputDecoration(
+                icon: const Icon(Icons.person),
+                hintText: '相手の名前',
+                labelText: 'Name'
+              ),
+            ),
+            TextFormField(
+              decoration: new InputDecoration(
+                  icon: const Icon(Icons.person),
+                  hintText: '借りたもの、貸したもの',
+                  labelText: 'loan'
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(top: 8.0),
+              child: Text(
+                "締め切り日:${_data.date.toString().substring(0,10)}"
+              ),
+            ),
+            RaisedButton(
+              child: const Text("締め切り日変更"),
+              onPressed: (){
+                print("sima kiri ");
+              },
+            )
+
+          ],
+        ),
+      )),
+    );
+  }
 }
