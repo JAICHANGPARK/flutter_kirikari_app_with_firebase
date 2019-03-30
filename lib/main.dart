c9410a99aa93edcb9572f58fe6772b7b941d6530
Dreamwalker <aristojeff@gmail.com>
2019-03-30 14:47:24 +0900

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -80,7 +80,7 @@ class _MyHomePageState extends State<MyHomePage> {
             leading: const Icon(Icons.memory),
             title: Text("【 " + (document['borrowOrLend'] == "lend"?"貸":"借" +
             " 】" + document['stuff'])),
-            subtitle: Text(" 期限 : " + document['data'].toString()
+            subtitle: Text(" 期限 : " + document['date'].toString().substring(0, 10)
             + "\n 相手 : " + document['user']),
           )
         ],
